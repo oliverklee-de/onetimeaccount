63e56b2294618691d281198040a6feb88edeff28
Oliver Klee <typo3-coding@oliverklee.de>
2026-04-28 12:28:09 +0200

    [TASK] Require oelib >= 6.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
             'extbase' => '11.5.41-12.4.99',
             'fluid' => '11.5.41-12.4.99',
             'feuserextrafields' => '6.7.1-7.99.99',
-            'oelib' => '6.2.0-6.99.99',
+            'oelib' => '6.3.0-6.99.99',
         ],
         'conflicts' => [
         ],
