ce3c45126365007a920b5aaf195edfd172723dc6
Oliver Klee <typo3-coding@oliverklee.de>
2026-05-14 20:51:02 +0200

    [TASK] Require TYPO3 >= 8.1 in `ext_emconf.php`

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -61,7 +61,7 @@ return RectorConfig::configure()
     ])
     ->withImportNames(true, true, false)
     ->withConfiguredRule(ExtEmConfRector::class, [
-        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '7.4.0-8.4.99',
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.4.99',
         ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.41-12.4.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
